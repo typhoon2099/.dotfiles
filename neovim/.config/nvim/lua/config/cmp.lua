ea970c6de0d925998962c5b4ad0c0be100655308
Matthew Simpson <matthew@matthewsimpson.net>
2023-12-29 15:31:02 +0000

    Use tab to select completions, select first option automatically

--- neovim/.config/nvim/lua/config/cmp.lua
+++ neovim/.config/nvim/lua/config/cmp.lua
@@ -15,7 +15,7 @@ cmp.setup({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     }),
-    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
@@ -27,6 +27,9 @@ cmp.setup({
     { name = 'buffer' },
   })
 })
+
+-- Use Enter to select completion items
+vim.api.nvim_set_keymap('i', '<CR>', 'pumvisible() ? "\\<C-y>" : "\\<C-g>u\\<CR>"', { noremap = true, silent = true, expr = true })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline('/', {
