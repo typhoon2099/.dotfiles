a718639886a2a6bce7b2f5ee698baab1f4a604b3
Matthew Simpson <matthew@matthewsimpson.net>
2024-01-03 15:05:22 +0000

    Autoformat

--- neovim/.config/nvim/lua/config/treesitter.lua
+++ neovim/.config/nvim/lua/config/treesitter.lua
@@ -1,4 +1,4 @@
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
   ensure_installed = { "ruby", "lua", "rust", "javascript", "html", "css", "svelte" },
   sync_install = false,
   auto_install = true,
