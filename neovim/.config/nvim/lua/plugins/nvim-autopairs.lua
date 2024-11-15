174fba57e5bb7dbd3997e2083b95199ae69b7b47
Matthew Simpson <matthew@matthewsimpson.net>
2024-11-15 15:11:37 +0000

    Make autopairs Lazy, add Lua

--- neovim/.config/nvim/lua/plugins/nvim-autopairs.lua
+++ neovim/.config/nvim/lua/plugins/nvim-autopairs.lua
@@ -1,9 +1,11 @@
 return {
   'windwp/nvim-autopairs',
+  event = 'InsertEnter',
   config = function()
     local npairs = require("nvim-autopairs")
     npairs.setup {}
 
     npairs.add_rules(require('nvim-autopairs.rules.endwise-ruby'))
+    npairs.add_rules(require('nvim-autopairs.rules.endwise-lua'))
   end,
 }
