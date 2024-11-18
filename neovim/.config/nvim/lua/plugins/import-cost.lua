31ce982ae2ad18adad6abfad357432943ec6f89b
Matthew Simpson <matthew@matthewsimpson.net>
2024-11-18 14:18:30 +0000

    Make import-cost.nvim Lazy

--- neovim/.config/nvim/lua/plugins/import-cost.lua
+++ neovim/.config/nvim/lua/plugins/import-cost.lua
@@ -1,7 +1,9 @@
 return {
   'barrett-ruth/import-cost.nvim',
+  ft = {
+    'javascript',
+    'javascriptreact'
+  },
   build = 'sh install.sh yarn',
-  config = function()
-    require('import-cost').setup()
-  end
+  config = true,
 }
