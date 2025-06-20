91fee25482b9db59ffa91441ecf6073e56baf00d
Matthew Simpson <matthew@matthewsimpson.net>
2025-06-20 15:59:18 +0100

    Correct Telescope borders

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -26,6 +26,9 @@ return {
     vim.keymap.set('n', '<leader>ee', builtin.symbols, {})
   end,
   opts = {
+    defaults = {
+      border = false,
+    },
     pickers = {
       find_files = {
         hidden = true,
