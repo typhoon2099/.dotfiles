3f5be8f76dacf8cfc2df6eb54add1a1d905d1591
Matthew Simpson <matthew@matthewsimpson.net>
2025-11-09 13:10:43 +0000

    Add `mini.move` for line/block moving

---
+++ neovim/.config/nvim/lua/plugins/mini.move.lua
@@ -0,0 +1,18 @@
+return {
+  'nvim-mini/mini.move',
+  version = '*',
+  lazy = false,
+  opts = {
+    mappings = {
+      left       = '<S-left>',
+      right      = '<S-right>',
+      down       = '<S-down>',
+      up         = '<S-up>',
+
+      line_left  = '<S-left>',
+      line_right = '<S-right>',
+      line_down  = '<S-down>',
+      line_up    = '<S-up>',
+    }
+  }
+}
